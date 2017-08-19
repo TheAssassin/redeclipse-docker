a2d7a6d18b79ffe5d2aff016db7073b42df23496
TheAssassin <theassassin@users.noreply.github.com>
2017-08-19 12:07:14 +0200

    Improve build script

--- build.sh
+++ build.sh
@@ -2,11 +2,22 @@
 
 # export BRANCH and/or COMMIT to modify the build process
 
+log() { echo $(tput setaf 2)$(tput bold)"$*"$(tput sgr0) ; }
+
 branch="${BRANCH:=master}"
 commit="${COMMIT:=$(curl -s https://api.github.com/repos/red-eclipse/base/commits/$branch | jq -r '.sha')}"
+
+log "Building image for branch $branch, commit $commit"
+echo
 
 image_name="quay.io/theassassin/redeclipse"
 
 docker build --build-arg COMMIT="$commit" --build-arg BRANCH="$branch" -t "$image_name:$commit" -t "$image_name:$branch" .
-docker push "$image_name:$commit"
-docker push "$image_name:$branch"
+
+if [ "$1" == "--push" ]; then
+    log "Pushing to quay.io"
+    echo
+
+    docker push "$image_name:$commit"
+    docker push "$image_name:$branch"
+fi
