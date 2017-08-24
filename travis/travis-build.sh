9a5819336252223ad0235cb32cd0823ac09715d9
TheAssassin <theassassin@users.noreply.github.com>
2017-08-24 22:12:27 +0200

    Build both branches in cron job
    Unfortunately, one can not set more than one cron job in Travis (and,
    more importantly, modify environment variables). Thus, cron jobs should
    just build all branches.

--- travis/travis-build.sh
+++ travis/travis-build.sh
@@ -2,29 +2,34 @@
 
 set -x
 
-# first of all, log in
-docker login -u "$QUAY_USER" -p "$QUAY_PASS" quay.io
-
-# if the build does not have a BRANCH environment variable, we'll set "master" as default
-# to allow for checking whether the relevant commit is on the registry already
-export BRANCH="${BRANCH:=master}"
-
-# set the commit
-export COMMIT="${COMMIT:=$(curl -s https://api.github.com/repos/red-eclipse/base/commits/$branch | jq -r '.sha')}"
-
-# then, check whether it's a cron build
-if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
-    # it's a cron build, so make sure we don't overwrite an existing tag on the registry
-    # that would be pretty annoying, as it'd modify the image hash nevertheless, causing
-    # unnecessary updates for users who update regularly
-    if curl --silent https://quay.io/v1/repositories/theassassin/redeclipse/tags | grep -q "$COMMIT"; then
-        echo "Image for commit $COMMIT exists on registry -- skipping build"
-        exit 0
-    fi
+# if the build does not have a BRANCH environment variable, we'll build for both
+# branches
+if [ "$BRANCH" != "" ]; then
+    BRANCHES=( "$BRANCH" )
+else
+    BRANCHES=( "master" "stable" )
 fi
 
-# either it's not a cron build or the tag is missing on the registry
-# thus, we can build and push the image
-# if the build is triggered externally, the override is probably intentional, e.g. when
-# this repository has been updated, and an older tag needs to be updated asap
-exec bash build.sh --push
+for branch in "${BRANCHES}"; do
+    # to allow for checking whether the relevant commit is on the registry already
+    export BRANCH="$branch"
+    # set the commit
+     export COMMIT="${COMMIT:=$(curl -s https://api.github.com/repos/red-eclipse/base/commits/$branch | jq -r '.sha')}"
+
+    # then, check whether it's a cron build
+    if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
+        # it's a cron build, so make sure we don't overwrite an existing tag on the registry
+        # that would be pretty annoying, as it'd modify the image hash nevertheless, causing
+        # unnecessary updates for users who update regularly
+        if curl --silent https://quay.io/v1/repositories/theassassin/redeclipse/tags | grep -q "$COMMIT"; then
+            echo "Image for commit $COMMIT exists on registry -- skipping build"
+            exit 0
+        fi
+    fi
+
+    # either it's not a cron build or the tag is missing on the registry
+    # thus, we can build and push the image
+    # if the build is triggered externally, the override is probably intentional, e.g. when
+    # this repository has been updated, and an older tag needs to be updated asap
+    exec bash build.sh --push
+done
